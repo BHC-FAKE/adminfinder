d68fddcaa48a0be73efe9354f1f8a9017df5c815
BLACK HAT CYBER <51839125+BHC-FAKE@users.noreply.github.com>
2019-10-31 15:25:51 +0700

    Add files via upload

---
+++ finder.php
@@ -0,0 +1,52 @@
+<?php
+// Author Name : ./IFake
+// Information List : 4001 list
+// Instagram :~# fakegajee
+// greetz : BlackHatCyber - We Are Fake - Vanessa Attacker 99
+
+print "   
+	
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                         
+                Admin Finder - coded by ./IFake
+  Information list : 4001 list
+  Thanks to  :BlackHatCyber - We Are Fake - Vanessa Attacker 99
+";
+
+echo "masukan site  : ";
+$target = trim(fgets(STDIN));
+$list = "ifake_wordlist.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
+}
+
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
+
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\e[1;32m\n\n [".date('H:m:s')."\e[1;32m] Mencoba : $log => Ditemukan\n";
+	}else{
+		print "\e[1;31m\n[".date('H:m:s')."\e[1;31m] Mencoba : $log => tidak di temukan";
+	}
+}
+
+?>
